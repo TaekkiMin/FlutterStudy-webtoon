61be2ed374c0485c5d0092ef7c262095513791f7
Taek Ki Min <127868094+TaekkiMin@users.noreply.github.com>
2024-01-09 17:56:29 +0900

    episodes 회차 버튼 만들기

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -41,82 +41,138 @@ class _DetailScreenState extends State<DetailScreen> {
           style: const TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
         ),
       ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 50,
+      body: SingleChildScrollView(
+        // 위젯이 overflow 시 사용
+        child: Padding(
+          // 전체 body 내용 padding 50 설정
+          padding: const EdgeInsets.symmetric(
+            horizontal: 50,
+            vertical: 50,
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: Column(
             children: [
-              Hero(
-                tag: widget.id, // 여기서 id는 webtoon의 id
-                child: Container(
-                  width: 250,
-                  clipBehavior: Clip.hardEdge,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 15, // 그림자 지름(크기) 설정
-                        offset: const Offset(10, 10), // 그림자 위치 설정
-                        color: Colors.black.withOpacity(0.3), // 그림자 색깔과 투명도 설정
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Hero(
+                    tag: widget.id, // 여기서 id는 webtoon의 id
+                    child: Container(
+                      width: 250,
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 15, // 그림자 지름(크기) 설정
+                            offset: const Offset(10, 10), // 그림자 위치 설정
+                            color:
+                                Colors.black.withOpacity(0.3), // 그림자 색깔과 투명도 설정
+                          ),
+                        ],
                       ),
-                    ],
+                      child: Image.network(
+                        // image의 src를 통해 이미지 썸네일 출력
+                        widget.thumb,
+                        headers: const {
+                          "User-Agent":
+                              "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                        },
+                      ),
+                    ),
                   ),
-                  child: Image.network(
-                    // image의 src를 통해 이미지 썸네일 출력
-                    widget.thumb,
-                    headers: const {
-                      "User-Agent":
-                          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-                    },
-                  ),
-                ),
+                ],
+              ),
+
+              const SizedBox(
+                height: 25,
+              ),
+
+              // 웹툰 상세 정보
+              FutureBuilder(
+                future: webtoon, // WebtoonDetailModel 클래스 타입
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // 웹툰 about 출력
+                        Text(
+                          snapshot.data!.about,
+                          style: const TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+
+                        const SizedBox(
+                          height: 20,
+                        ),
+
+                        // 웹툰 genre, age 출력
+                        Text(
+                          '${snapshot.data!.genre} / ${snapshot.data!.age}',
+                          style: const TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    );
+                  }
+                  return const Text('...');
+                },
+              ),
+
+              const SizedBox(
+                height: 20,
+              ),
+
+              // 웹툰 에피소드
+              FutureBuilder(
+                future: episode,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                      // list 개수가 정해져있을 시 Column 사용
+                      children: [
+                        for (var episode in snapshot.data!)
+                          Container(
+                            // 버튼 사이의 margin 설정
+                            margin: const EdgeInsets.only(
+                              bottom: 10,
+                            ),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              color: Colors.green.shade400,
+                            ),
+                            // 버튼 안에 텍스트와 아이콘에 대한 padding 설정
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 20,
+                              vertical: 10,
+                            ),
+                            child: Row(
+                              // 텍스트와 아이콘에 대한 spacebetween으로 설정
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  episode.title,
+                                  style: const TextStyle(
+                                    fontSize: 15,
+                                  ),
+                                ),
+                                const Icon(
+                                  Icons.chevron_right_outlined,
+                                ),
+                              ],
+                            ),
+                          ),
+                      ],
+                    );
+                  }
+                  return Container();
+                },
               ),
             ],
           ),
-          const SizedBox(
-            height: 25,
-          ),
-          FutureBuilder(
-            future: webtoon, // WebtoonDetailModel 클래스 타입
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 50,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      // 웹툰 about 출력
-                      Text(
-                        snapshot.data!.about,
-                        style: const TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-
-                      const SizedBox(
-                        height: 10,
-                      ),
-
-                      // 웹툰 genre, age 출력
-                      Text(
-                        '${snapshot.data!.genre} / ${snapshot.data!.age}',
-                        style: const TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              }
-              return const Text('...');
-            },
-          ),
-        ],
+        ),
       ),
     );
   }
